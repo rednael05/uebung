5a62f40766917da60e250f76d2b6e989a4734401
Leander <leander@bonsch.de>
2020-02-15 21:05:50 +0100

    alter akinator

--- spiel.py
+++ spiel.py
@@ -1,22 +1,42 @@
 from random import random, randint
 
-print("wikommen zum spiel schere stein papier \n 1=schere \n 2=stein \n 3=papier \n 4=brunnen ")
-print("wähle deinen zug:")
-zp = 0
-zc = 0
-while True:
-    spieler = int(input())
-    if spieler == "x":
-        exit(0)
-    computer = randint(1, 4)
-    if spieler == computer:
-        print("unentschieden")
-    elif (spieler == 1 & computer == 3) | (spieler == 2 & computer == 1) | (
-            spieler == 3 & computer == 2 | computer == 4) | (spieler == 4 & computer == 1 | computer == 2):
-        print("glückwunsch du hast gewonnen")
-        zp = zp + 1
-    else:
-        print("du hast leider verloren versuche es beim nächsten mal nochmal")
-        zc = zc + 1
-    print("Computer: " + computer + "\n Spieler: " + spieler)
 
+def translate(zahl):
+    if zahl == 1:
+        return "Schere: "+str(zahl)
+    elif zahl == 2:
+        return "Stein: "+str(zahl)
+    elif zahl == 3:
+        return "Papier: "+str(zahl)
+    elif zahl == 4:
+        return "Brunnen: "+str(zahl)
+
+
+if __name__ == '__main__':
+    print("wikommen zum spiel schere stein papier \n 1=schere \n 2=stein \n 3=papier \n 4=brunnen ")
+    print("wähle deinen zug:")
+    zp = 0
+    zc = 0
+    while True:
+        i = input()
+        if i == "x":
+            exit(0)
+        spieler = int(i)
+        if spieler < 1 or spieler > 4:
+            print("ungültige eingabe")
+            continue
+        computer = randint(1, 4)
+
+        if spieler == computer:
+            print("unentschieden")
+        elif (spieler == 1 and computer == 3) or \
+                (spieler == 2 and computer == 1) or\
+                (spieler == 3 and (computer == 2 or computer == 4)) or\
+                (spieler == 4 and (computer == 1 or computer == 2)):
+            print("glückwunsch du hast gewonnen: "+str(spieler)+":"+str(computer))
+            zp = zp + 1
+        else:
+            print("du hast leider verloren versuche es beim nächsten mal nochmal")
+            zc = zc + 1
+        print("Computer: " + translate(computer) + "\n Spieler: " + translate(spieler))
+        print("deine punkte " + str(zp) + " die punkte vom computer " + str(zc))
